a1f4d789b9830a1c0b0c2171d80cd64fbdc70427
Sébastien Elet <sebastien@elet.fr>
2020-12-06 17:54:31 +0100

    feat(advent): add day1

---
+++ 2020-advent-of-code/day1.test.ts
@@ -0,0 +1,43 @@
+import { findTupleExpensesMatching, findTripleExpensesMatching } from "./day1";
+
+describe("day1", () => {
+  describe("findTupleExpensesmatching", () => {
+    describe("with no match", () => {
+      test("should return 0", () => {
+        expect(findTupleExpensesMatching(30)([])).toEqual(0);
+        expect(findTupleExpensesMatching(30)([15])).toEqual(0);
+      });
+    });
+    describe("with one match", () => {
+      test("should return 200", () => {
+        expect(findTupleExpensesMatching(30)([5, 10, 20, 40])).toEqual(200);
+      });
+    });
+    describe("with multiple match", () => {
+      test("should return 200 (the first match)", () => {
+        expect(findTupleExpensesMatching(30)([5, 10, 20, 12, 18])).toEqual(200);
+      });
+    });
+  });
+
+  describe("findTripleExpensesMatching", () => {
+    describe("with no match", () => {
+      test("should return 0", () => {
+        expect(findTripleExpensesMatching(30)([])).toEqual(0);
+        expect(findTripleExpensesMatching(30)([10])).toEqual(0);
+      });
+    });
+    describe("with one match", () => {
+      test("should return 750", () => {
+        expect(findTripleExpensesMatching(30)([5, 10, 15, 40])).toEqual(750);
+      });
+    });
+    describe("with multiple match", () => {
+      test("should return 200 (the first match)", () => {
+        expect(findTripleExpensesMatching(30)([5, 10, 15, 3, 9, 18])).toEqual(
+          750
+        );
+      });
+    });
+  });
+});
---
+++ 2020-advent-of-code/day1.ts
@@ -0,0 +1,65 @@
+const readExpense = (fileName: string) =>
+  require("fs")
+    .readFileSync(fileName)
+    .toString()
+    .split("\n")
+    .map((line: string) => line.trim())
+    .map(Number)
+    .filter((n: number) => n > 0);
+
+export const findTupleExpensesMatching = (sum: number) => (
+  expenses: number[]
+): number => {
+  let result = 0;
+
+  expenses.some((expense, firstIndex) =>
+    expenses.some((secondExpense, secondIndex) => {
+      if (firstIndex === secondIndex) {
+        return false;
+      }
+
+      const check = expense + secondExpense === sum;
+
+      if (check) {
+        result = expense * secondExpense;
+      }
+      return check;
+    })
+  );
+
+  return result;
+};
+
+export const findTripleExpensesMatching = (sum: number) => (
+  expenses: number[]
+) => {
+  let result = 0;
+
+  expenses.some((expense, firstIndex) =>
+    expenses.some(
+      (secondExpense, secondIndex) =>
+        firstIndex !== secondIndex &&
+        expenses.some((thirdExpense, thirdIndex) => {
+          if (secondIndex === thirdIndex || firstIndex === thirdIndex) {
+            return false;
+          }
+          const check = expense + secondExpense + thirdExpense === sum;
+
+          if (check) {
+            result = expense * secondExpense * thirdExpense;
+          }
+          return check;
+        })
+    )
+  );
+
+  return result;
+};
+
+if (require.main === module) {
+  const expenses = readExpense("day1.txt");
+  const findTupleExpensesMatching2020 = findTupleExpensesMatching(2020);
+  console.log(findTupleExpensesMatching2020(expenses));
+  const findTripleExpensesMatching2020 = findTripleExpensesMatching(2020);
+  console.log(findTripleExpensesMatching2020(expenses));
+}
