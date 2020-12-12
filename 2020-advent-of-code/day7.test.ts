da31010aa3237b0733371910a93898388d06af57
Sébastien Elet <sebastien@elet.fr>
2020-12-12 16:55:25 +0100

    feat(advent): add day7

---
+++ 2020-advent-of-code/day7.test.ts
@@ -0,0 +1,151 @@
+import { howManyCanCarry, howManyRequiredInside, parseRules } from "./day7";
+
+const rules = `
+  light red bags contain 1 bright white bag, 2 muted yellow bags.
+  dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+  bright white bags contain 1 shiny gold bag.
+  muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+  shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+  dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+  vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+  faded blue bags contain no other bags.
+  dotted black bags contain no other bags.
+`;
+describe("day7", () => {
+  describe("parseRules", () => {
+    test("should return an array of rules", () => {
+      expect(parseRules(rules)).toMatchInlineSnapshot(`
+        Array [
+          Array [
+            "light red",
+            Array [
+              Object {
+                "bag": "bright white",
+                "quantity": "1",
+              },
+              Object {
+                "bag": "muted yellow",
+                "quantity": "2",
+              },
+            ],
+          ],
+          Array [
+            "dark orange",
+            Array [
+              Object {
+                "bag": "bright white",
+                "quantity": "3",
+              },
+              Object {
+                "bag": "muted yellow",
+                "quantity": "4",
+              },
+            ],
+          ],
+          Array [
+            "bright white",
+            Array [
+              Object {
+                "bag": "shiny gold",
+                "quantity": "1",
+              },
+            ],
+          ],
+          Array [
+            "muted yellow",
+            Array [
+              Object {
+                "bag": "shiny gold",
+                "quantity": "2",
+              },
+              Object {
+                "bag": "faded blue",
+                "quantity": "9",
+              },
+            ],
+          ],
+          Array [
+            "shiny gold",
+            Array [
+              Object {
+                "bag": "dark olive",
+                "quantity": "1",
+              },
+              Object {
+                "bag": "vibrant plum",
+                "quantity": "2",
+              },
+            ],
+          ],
+          Array [
+            "dark olive",
+            Array [
+              Object {
+                "bag": "faded blue",
+                "quantity": "3",
+              },
+              Object {
+                "bag": "dotted black",
+                "quantity": "4",
+              },
+            ],
+          ],
+          Array [
+            "vibrant plum",
+            Array [
+              Object {
+                "bag": "faded blue",
+                "quantity": "5",
+              },
+              Object {
+                "bag": "dotted black",
+                "quantity": "6",
+              },
+            ],
+          ],
+          Array [
+            "faded blue",
+            Array [],
+          ],
+          Array [
+            "dotted black",
+            Array [],
+          ],
+        ]
+      `);
+    });
+  });
+
+  describe("howManyCanCarry", () => {
+    test("should return 4", () => {
+      expect(howManyCanCarry(rules, "shiny gold")).toEqual(4);
+    });
+  });
+
+  describe("howManyRequiredInside", () => {
+    describe("with example", () => {
+      test("should return 32", () => {
+        expect(howManyRequiredInside(rules, "shiny gold")).toEqual(32);
+      });
+    });
+
+    describe("with another example", () => {
+      test("should return 126", () => {
+        expect(
+          howManyRequiredInside(
+            `
+            shiny gold bags contain 2 dark red bags.
+            dark red bags contain 2 dark orange bags.
+            dark orange bags contain 2 dark yellow bags.
+            dark yellow bags contain 2 dark green bags.
+            dark green bags contain 2 dark blue bags.
+            dark blue bags contain 2 dark violet bags.
+            dark violet bags contain no other bags.
+            `,
+            "shiny gold"
+          )
+        ).toEqual(126);
+      });
+    });
+  });
+});
---
+++ 2020-advent-of-code/day7.ts
@@ -0,0 +1,105 @@
+const readFile = (fileName: string) =>
+  require("fs").readFileSync(fileName).toString();
+const trim = (input: string) => input.trim();
+const split = (separator: string) => (input: string) => input.split(separator);
+const splitRules = split("\n");
+const length = <T>(input: T[]) => input.length;
+const replace = (predicate: string | RegExp, replacement: string) => (
+  input: string
+) => input.replace(predicate, replacement);
+const removeBags = replace(/ bag(s?)(\.?)/, "");
+export const parseRules = (input: string) => {
+  const rules = splitRules(trim(input))
+    .map(trim)
+    .map(split("contain"))
+    .map(([bag, contain]) => {
+      return [
+        trim(removeBags(bag)),
+        split(",")(contain)
+          .map(trim)
+          .map(removeBags)
+          .filter((item) => item !== "no other")
+          .map((item) => {
+            if (/^\d/.test(item)) {
+              const [quantity] = item.match(/\d/) || ["0"];
+              const bag = trim(replace(quantity, "")(item));
+              return { quantity, bag };
+            }
+            return null;
+          })
+          .filter(Boolean),
+      ];
+    });
+  return rules;
+};
+
+export const howManyCanCarry = (rulesInput: string, input: string) => {
+  const rules = parseRules(rulesInput);
+  function canCarryFromRule(rule: any) {
+    if (!rule) return null;
+    const [bagColor, carry] = rule;
+    if (!carry) return null;
+    const canCarry = carry.find((item: any) => {
+      if (item.bag === input) {
+        return true;
+      }
+    });
+
+    if (!canCarry) {
+      const subCarry = carry
+        .map((item: any) => {
+          const r = rules.find((i: any) => {
+            return i[0] === item.bag;
+          });
+
+          return canCarryFromRule(r);
+        })
+        .filter(Boolean);
+
+      if (subCarry.length === 0) {
+        return null;
+      }
+    }
+
+    return bagColor;
+  }
+  const colors = rules.map(canCarryFromRule).filter(Boolean);
+
+  return length(colors);
+};
+export const howManyRequiredInside = (rulesInput: string, input: string) => {
+  const rules = parseRules(rulesInput);
+  function quantityFromRule(rule: any) {
+    if (!rule) return 0;
+    const [bagColor, carry] = rule;
+    if (!carry) return 0;
+
+    return carry.reduce((sum: number, c: any) => {
+      const r = rules.find((i: any) => {
+        return i[0] === c.bag;
+      });
+      if (Number(c.quantity) > 0) {
+        sum += Number(c.quantity) * (quantityFromRule(r) + 1);
+      }
+
+      return sum;
+    }, 0);
+  }
+  const rule = rules.find((i: any) => {
+    return i[0] === input;
+  });
+  const sum = quantityFromRule(rule);
+  return sum;
+};
+
+if (require.main === module) {
+  const rules = readFile("day7.txt");
+  console.log(
+    "How many bag colors can eventually contain at least one shiny gold bag",
+    howManyCanCarry(rules, "shiny gold")
+  );
+  console.log(
+    "How many individual bags are required inside your single shiny gold bag?",
+    howManyRequiredInside(rules, "shiny gold")
+  );
+}
